787f197e3e484f30f96ee383088ad51351ca62d0
2lu3 <hi2lu3@gmail.com>
2019-06-08 21:52:52 +0900

    masuda.hppなどをすべてこめんとあうと

--- code(c++)/Masuda.hpp
+++ code(c++)/Masuda.hpp
@@ -1,20 +1,20 @@
-#ifndef MASUDA
-#define MASUDA
+// #ifndef MASUDA
+// #define MASUDA
 
-#include "CospaceSettings.hpp"
-#include "CommonTools.hpp"
+// #include "CospaceSettings.hpp"
+// #include "CommonTools.hpp"
 
-// World1
-class Game0_Masuda : public UserGame0
-{
-public:
-	void setup(void) override;
-	void loop(void) override;
-	int shouldTeleport(void) override;
-	void taskOnTeleport(void) override;
+// // World1
+// class Game0_Masuda : public UserGame0
+// {
+// public:
+// 	void setup(void) override;
+// 	void loop(void) override;
+// 	int shouldTeleport(void) override;
+// 	void taskOnTeleport(void) override;
 
-private:
-};
+// private:
+// };
 
 
-#endif // !MASUDA
+// #endif // !MASUDA
