d90af61f8ff2edaab4e2e11c3f58c66f4f7013b3
2lu3 <hi2lu3@gmail.com>
2019-07-06 14:44:44 +1000

    試合終了の直前には、オブジェクトをとりながら移動しないようにした

--- code(c++)/Hikaru.cpp
+++ code(c++)/Hikaru.cpp
@@ -316,7 +316,7 @@ void Game1_Hikaru::loop()
 				if (PositionY < 80 && next_allowed_go_time[RED_LOADED_ID][1] <= Time) {
 					process = 1;
 				}
-				else if(PositionY >= 180 && next_allowed_go_time[RED_LOADED_ID][0] <= Time) {
+				else if (PositionY >= 180 && next_allowed_go_time[RED_LOADED_ID][0] <= Time) {
 					process = 0;
 				}
 				else {
@@ -326,7 +326,7 @@ void Game1_Hikaru::loop()
 					else {
 						process = 0;
 					}
- 				}
+				}
 				process_times = 0;
 			}
 			if (process == 0)
@@ -369,7 +369,7 @@ void Game1_Hikaru::loop()
 				{
 					process = 1;
 				}
-				else if(PositionY >= 60 && next_allowed_go_time[CYAN_LOADED_ID][0] <= Time)
+				else if (PositionY >= 60 && next_allowed_go_time[CYAN_LOADED_ID][0] <= Time)
 				{
 					process = 0;
 				}
@@ -880,8 +880,8 @@ void Game1_Hikaru::InputDotInformation(void)
 			switch (map_output_data[kDotHeightNum - j - 1][i])
 			{
 			case 0: //white
-				map_position_color_data[i][j] = POINT_UNKNOWN;
-				//map_position_color_data[i][j] = POINT_WHITE;
+				//map_position_color_data[i][j] = POINT_UNKNOWN;
+				map_position_color_data[i][j] = POINT_WHITE;
 				break;
 			case 1: //yellow
 				map_position_color_data[i][j] = POINT_YELLOW;
@@ -1207,13 +1207,7 @@ void Game1_Hikaru::Dijkstra(int option)
 
 			target_cost += target_curved_times * 10;
 
-			if (LoadedObjects >= 6)
-			{
-			}
-			else
-			{
-				target_cost += static_cast<int>(dot[target_id].arrived_times * 10);
-			}
+			
 			if (dot[i].point == POINT_SWAMPLAND || dot[i].point == POINT_MAY_SWAMPLAND)
 			{
 				cout << "s" << endl;
@@ -1226,29 +1220,38 @@ void Game1_Hikaru::Dijkstra(int option)
 			// 	// }
 			// }
 			double k = 0.8;
-			if (dot[investigating_node.id].black == 1 && loaded_objects[BLACK_LOADED_ID] < kBorderSameObjNum)
-			{
-				if (searching_object == BLACK_LOADED_ID)
+			if (Time < 450) {
+				if (dot[investigating_node.id].black == 1 && loaded_objects[BLACK_LOADED_ID] < kBorderSameObjNum)
 				{
-					target_cost = static_cast<int>((k - 0.3) * target_cost);
+					if (searching_object == BLACK_LOADED_ID)
+					{
+						target_cost = static_cast<int>((k - 0.3) * target_cost);
+					}
+					target_cost = static_cast<int>(k * target_cost);
 				}
-				target_cost = static_cast<int>(k * target_cost);
-			}
-			if (dot[investigating_node.id].cyan == 1 && loaded_objects[CYAN_LOADED_ID] < kBorderSameObjNum)
-			{
-				if (searching_object == CYAN_LOADED_ID)
+				if (dot[investigating_node.id].cyan == 1 && loaded_objects[CYAN_LOADED_ID] < kBorderSameObjNum)
 				{
-					target_cost = static_cast<int>((k - 0.3) * target_cost);
+					if (searching_object == CYAN_LOADED_ID)
+					{
+						target_cost = static_cast<int>((k - 0.3) * target_cost);
+					}
+					target_cost = static_cast<int>(k * target_cost);
 				}
-				target_cost = static_cast<int>(k * target_cost);
-			}
-			if (dot[investigating_node.id].red == 1 && loaded_objects[RED_LOADED_ID] < kBorderSameObjNum)
-			{
-				if (searching_object == RED_LOADED_ID)
+				if (dot[investigating_node.id].red == 1 && loaded_objects[RED_LOADED_ID] < kBorderSameObjNum)
 				{
-					target_cost = static_cast<int>((k - 0.3) * target_cost);
+					if (searching_object == RED_LOADED_ID)
+					{
+						target_cost = static_cast<int>((k - 0.3) * target_cost);
+					}
+					target_cost = static_cast<int>(k * target_cost);
 				}
-				target_cost = static_cast<int>(k * target_cost);
+				if (LoadedObjects >= 6)
+				{
+				}
+				else
+				{
+					target_cost += static_cast<int>(dot[target_id].arrived_times * 10);
+				}
 			}
 
 			if (target_cost <= 0)
